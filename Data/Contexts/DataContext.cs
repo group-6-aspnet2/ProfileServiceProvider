6f8e83d840d104fa6f36d22700922c0e00686c76
Olivia Olsson <olivia.olsson@yh.nackademin.se>
2025-05-15 18:00:46 +0200

    Add UserProfileEntity and register it in DataContext

--- Data/Contexts/DataContext.cs
+++ Data/Contexts/DataContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Contexts;
 
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
+    public DbSet<UserProfileEntity> UserProfiles { get; set; }
 }
